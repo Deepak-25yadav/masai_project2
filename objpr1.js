ffc163d566f32f3b592b5742004a81030ab9bf01
Deepak Yadav <25yadavdeepak@gmail.com>
2022-10-09 11:20:50 +0530

    it is pushing from location folder10 inside folder10 available this masai_project2

---
+++ objpr1.js
@@ -0,0 +1,29 @@
+let arr=["rohan","rohit","ajay","vijay","ajit"];
+//console.log(arr);
+console.log(arr[2]);
+
+
+
+
+
+
+
+let diary={
+name:"Deepak",
+  contact:212405,
+  age:22,
+  location:"bhopal",
+  hobbies:["travelling","dancing","singing","playing"]
+ 
+}
+
+//console.log(diary['hobbies']);
+  //console.log(diary);
+
+
+//console.log(diary['name']); 
+
+//for targetting any specific value as output so that value's key take inside single or inside double qoute because key always would be string.
+             
+             
+            
